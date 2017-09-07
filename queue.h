d4c35da4ba64b6bc2be2cf8cff03b4b28f246764
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2017-09-07 18:43:22 -0300

    Application working properly on macOS
    Changes in md5 command (to md5sum) should be made to port it to linux

--- queue.h
+++ queue.h
@@ -5,13 +5,10 @@
 
 typedef struct taskQueueCDT *taskQueue_t;
 
-	/* Return a pointer to an empty stack. */
 	extern taskQueue_t newTaskQueue();
-	/* Push value onto the stack, returning success flag. */
 	extern void offer(taskQueue_t q, task_t task);
-	/* Pop value from the stack, returning success flag. */
 	extern task_t * poll(taskQueue_t q);
+	extern int size(taskQueue_t q);
 	extern uint8_t isEmpty(taskQueue_t q);
-
 
 #endif
--- slaveProcess.c
+++ slaveProcess.c
@@ -1,97 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "task.h"
 #include "slaveProcess.h"
 
-int main(int argc, char *argv[])
+void processTask(task_t * task);
+
+int main(int argc, char * argv[])
 {
+  char msg[300];
+	char * auxBuffer;
+  char * curr = msg;
+	char running = 1;
+	task_t auxTask;
 
-	uint8_t running = 1;
+	auxTask.filename = (char *)calloc(256, sizeof(char));
 
-  taskQueue_t unprocessedTasks = newTaskQueue();
-  taskQueue_t processedTasks;
+	auxBuffer = (char *)calloc(MAX_FILENAME_LEN + 1 + HASHSIZE + 2, sizeof(char));
 
-	task_t * auxTask;
-
-	int filenameLen;
-
-  //Acknowledge application
-  FILE * f = fopen(argv[1], "w");
-  fputs("ready", f);
-  fclose(f);
-
-	//Start reading commands
-	char * commandBuffer[512];
-
-	uint8_t state = WAITING_FOR_TASKS;
-
-	while(running)
-	{
-		//Wait and read command into buffer
-		f = fopen(argv[2], "r");
-		fgets(commandBuffer, 512, f);
-		fclose(f);
-		switch(state)
-		{
-			case WAITING_FOR_TASKS:
-				if(strncmp(commandBuffer, "<beginPayload>", 512)==0)
-				{
-					state = RECEVING_TASKS;
-				}
-				else if(strncmp(commandBuffer, "<killSlave>", 512)==0)
-				{
-					wait(NULL);
-					_exit(0);
-				}
-				break;
-			case RECEVING_TASKS:
-				if(strncmp(commandBuffer, "<endPayload>", 512)==0)
-				{
-					processedTasks = processAllTasks(unprocessedTasks);
-					state = TASKS_READY;
-				}
-				else if(strncmp(commandBuffer, "<killSlave>", 512)==0)
-				{
-					wait(NULL);
-					_exit(0);
-				}
-				else
-				{
-					auxTask = malloc(sizeof(task_t));
-					auxTask->filename = calloc(strnlen_s(c, 512), sizeof(char));
-					strncpy(auxTask->filename, commandBuffer, 512);
-					auxTask->processed = 0;
-					offer(unprocessedTasks, *auxTask);
-				}
-				break;
-			case TASKS_READY:
-				if(strncmp(commandBuffer, "<retreiveFinishedTasks>", 512)==0)
-				{
-					f = fopen(argv[1], "w");
-					while(!(isEmpty(processedTasks)))
-				  {
-				    auxTask = poll(processedTasks);
-				    fprintf(f, "%s:%s\n", auxTask->filename, auxTask->hashmd5);
-				  }
-					fclose(f);
-					state = WAITING_FOR_TASKS;
-				}
-				else if(strncmp(commandBuffer, "<killSlave>", 512)==0)
-				{
-					wait(NULL);
-					_exit(0);
-				}
-				break;
+	while(running) {
+		while((*curr=getchar()) && *curr!='\0') { curr++; }
+		switch(msg[0]) {
+			case 3: running = 0; /*fprintf(stderr, "Exit\n");*/ break;
+			case 2: strncpy(auxTask.filename, msg+1, 256);
+							processTask(&auxTask);
+							sprintf(auxBuffer, "%s:%s\n", auxTask.filename, auxTask.hashmd5);
+							write(STDOUT_FILENO, auxBuffer, strlen(auxBuffer));
 		}
+		curr = msg;
 	}
-  offer(q, t1);
-  offer(q, t2);
-  ret = processAllTasks(q);
-  task_t * aux;
-  while(!(isEmpty(ret)))
-  {
-    aux = poll(ret);
-    //printf("El hash md5 de %s es %s\n", aux->filename, aux->hashmd5);
-  }
-
   return 0;
+
 }
 
 void processTask(task_t * task)
@@ -113,7 +53,7 @@ void processTask(task_t * task)
     close(pipefd[0]);
     close(pipefd[1]);
     /* Ejecuto md5sum */
-    execlp("md5sum", "md5sum", task->filename, NULL);
+		execlp("md5", "md5", "-q",task->filename, NULL);
   }
   else
   {
@@ -129,17 +69,4 @@ void processTask(task_t * task)
     close(pipefd[1]);
     return;
   }
-}
-
-taskQueue_t processAllTasks(taskQueue_t q)
-{
-  taskQueue_t ret = newTaskQueue();
-  task_t * aux;
-  while (!isEmpty(q))
-  {
-    aux = poll(q);
-    processTask(aux);
-    offer(ret, *aux);
-  }
-  return ret;
 }
